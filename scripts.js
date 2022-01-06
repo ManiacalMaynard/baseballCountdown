2033149a75d977391e85188083317828032eadf1
ManiacalMaynard <maynard@maniacalindustries.com>
2022-01-05 16:23:36 -0800

    Adds toggle functionality to swap between major and minor league timer and changes image to reflect the current league. Play ball!

--- scripts.js
+++ scripts.js
@@ -1,23 +1,30 @@
-// This message will not self-destruct, ignore this console, it's just for me
+// This message will not self-destruct, ignore this console.log, it's just for me
 console.log('Because it\'s one, two, three strike you\'re out at the old ball game!')
 
-// Set the end of the timer, this will be modified by league and team choice
-let endDate = '';
-
-//function to select the league used for coundown calculation and display
+// These define the target dates for either league
 const majorOpeningDay = new Date("Mar 31, 2022 13:00:00").getTime();
 const minorOpeningDay = new Date("Apr 08, 2022 13:00:00").getTime();
 
+let endDate = majorOpeningDay;
 
-const selected = document.querySelector('selection');
-
+// Swaps which target date to use depending on choice. Note: assumes Major is selected on page load
 const changeLeague = () => {
-    // if major is chosen set endTime to equal majorOpeningDay, else euqal to minorOpeningDay
-    document.getElementById('majorLeagueSelected').classList.contains('.active') ?
-    endDate = majorOpeningDay : endDate = minorOpeningDay
+    let currentLeague = document.querySelector('.selected').id;
+    let majorImage = 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Major_League_Baseball_logo.svg/1280px-Major_League_Baseball_logo.svg.png'
+    let minorImage = 'https://upload.wikimedia.org/wikipedia/en/thumb/d/d1/MiLB_logo.svg/1280px-MiLB_logo.svg.png'
 
-}
-
+    if (currentLeague == 'majorLeagueSelect') {
+        document.getElementById('majorLeagueSelect').classList.remove('selected');
+        document.getElementById('minorLeagueSelect').classList.add('selected');
+        endDate = minorOpeningDay;
+        document.getElementById('leagueImage').src = minorImage;
+    } else {
+        document.getElementById('majorLeagueSelect').classList.add('selected');
+        document.getElementById('minorLeagueSelect').classList.remove('selected');
+        endDate = majorOpeningDay;
+        document.getElementById('leagueImage').src = majorImage;
+    };
+};
 
 //main function that ticks away, calculates the values, and chnages the divs
 //this might be better in a more modular set up, TODO
@@ -27,9 +34,8 @@ const timer = () => {
         let distance = endDate - rightNow;
         let days = Math.floor(distance / (1000 * 60 * 60 * 24));
         let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor( (distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor( (distance % (1000 * 60)) / 1000)
-
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
         document.getElementById('daysDisplay').innerText = days;
         document.getElementById('hoursDisplay').innerText = hours;
         document.getElementById('minsDisplay').innerText = minutes;
@@ -38,5 +44,4 @@ const timer = () => {
     }, 1000)
 }
 
-changeLeague()
 timer()
